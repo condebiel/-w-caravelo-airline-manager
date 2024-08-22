bd3a6838bc3ba48a4049ebcd301ba0ea7fc7a922
Biel Conde <bielbcm@gmail.com>
2024-08-22 15:48:52 +0200

    feat: Add UserProfileForm component

---
+++ components/UserProfileForm/UserProfileForm.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import TextInput from '@/components/TextInput/TextInput.vue'
+
+const mockForm = {
+  name: 'John',
+  surname: 'Doe',
+  email: 'biel@frontconde.com',
+  address: '1234 Main St',
+  city: 'Barcelona',
+  birthdate: '1987-02-23',
+  repository: 'github.com/condebiel',
+}
+</script>
+
+<template>
+  <div
+    class="
+      grid grid-cols-1 gap-8
+
+      md:grid-cols-2
+    "
+  >
+    <TextInput
+      id="name"
+      v-model="mockForm.name"
+      :label="$t('profile.name')"
+      disabled
+    />
+
+    <TextInput
+      id="surname"
+      v-model="mockForm.surname"
+      :label="$t('profile.surname')"
+      disabled
+    />
+
+    <TextInput
+      id="surname"
+      v-model="mockForm.email"
+      :label="$t('profile.email')"
+      disabled
+    />
+
+    <TextInput
+      id="address"
+      v-model="mockForm.address"
+      :label="$t('profile.address')"
+      disabled
+    />
+
+    <TextInput
+      id="city"
+      v-model="mockForm.city"
+      :label="$t('profile.city')"
+      disabled
+    />
+
+    <TextInput
+      id="birthdate"
+      v-model="mockForm.birthdate"
+      :label="$t('profile.birthdate')"
+      disabled
+    />
+
+    <TextInput
+      id="repository"
+      v-model="mockForm.repository"
+      :label="$t('profile.repository')"
+      disabled
+    />
+  </div>
+</template>
--- locales/en.ts
+++ locales/en.ts
@@ -1,10 +1,13 @@
 export default {
   profile: {
+    title: 'Client Profile',
     name: 'Name',
     surname: 'Surname',
     email: 'Email',
     address: 'Address',
     city: 'City',
     birthdate: 'Birthdate',
+    repository: 'Github',
+    editFlights: 'Edit Flights',
   },
 }
